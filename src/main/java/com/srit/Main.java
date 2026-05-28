2f4afa1cefd6bb2ec5ebe83e6a186acf6d72a800
Santosh Kumar <santosh.ksrit@gmail.com>
2026-05-28 11:14:59 +0530

    first change commit Testing

--- src/main/java/com/srit/Main.java
+++ src/main/java/com/srit/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("This is my first Java program.");
+        System.out.println("This is my second Java program");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint

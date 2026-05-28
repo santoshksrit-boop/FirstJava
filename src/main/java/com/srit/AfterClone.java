fbd3e725f2b2ec084cc9530bf6be6fcf8d6fa120
Santosh Kumar <santosh.ksrit@gmail.com>
2026-05-28 17:57:20 +0530

    added new file after clone

--- src/main/java/com/srit/AfterClone.java
+++ src/main/java/com/srit/AfterClone.java
@@ -4,5 +4,6 @@ public class AfterClone {
     public static void main(String[] args) {
         System.out.println("Testing data");
         System.out.println("Testing data values");
+        System.out.println("new line added");
     }
 }

c6ad04b27499a84de5d7cdf7e9a4fbe2c61c5a0c
Santosh Kumar <santosh.ksrit@gmail.com>
2026-05-28 11:44:04 +0530

    write the some code change commit Testing

--- src/main/java/com/srit/Demo.java
+++ src/main/java/com/srit/Demo.java
@@ -1,4 +1,13 @@
 package com.srit;
 
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("This is my first Java program.");
+        System.out.println("This is my second Java program");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
